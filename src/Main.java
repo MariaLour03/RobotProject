cbff1d93e4893b8500938529cb3a88e54369d535
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-11-14 10:02:10 -0600

    committed to the Java Robot code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Robot myRobot = new Robot();  // Create a new Robot object
+        myRobot.move();  // Make the Robot move
+        myRobot.checkBatteryLevel();  // Check the battery level
+        myRobot.stop();  // Stop the Robot
+        myRobot.chargeBattery();  // Charge the Robot's battery
+        myRobot.displayInfo(); //Displays Robot's name, color & weight
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
+
+
+
+
--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
 
 public class Robot {
+    int batteryLevel = 100;  // Battery level percentage (0 to 100)
+    int speed = 0;  // Current speed of the robot
+    String name = "Robo";  // Name of the robot
+    String color = "Red";  // Color of the robot
+    int weight = 50;  // Weight of the robot
+
+    //Method
+    public void move(){
+      speed = 5;
+      System.out.println(name + " is moving at speed " + speed + "mph.");
+    }
+
+    public void stop(){
+      speed = 0;
+      System.out.println(name + " has stopped");
+    }
+
+    public void chargeBattery(){
+      batteryLevel = 100;
+      System.out.println(name + "'s battery is fully charged.");
+    }
+
+    public void checkBatteryLevel(){
+      System.out.println(name + "'s current battery level is " + batteryLevel);
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: " + name + " Color: " + color + " Weight: " + weight);
+    }
 }
+
+
+
+
+
+
+
+
+
